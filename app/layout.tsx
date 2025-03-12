4ff6bc44257ac625078bb6156fb6596f37d556f3
jasonryan-bn <jason.ryan@brilliantnoise.com>
2025-03-12 16:33:55 +0000

    Update app layout, package files, and vercel configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,11 @@ import "../styles/bubbles.css";
 import MainComponent from "../components/MainComponent";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Korn Ferry - Workforce 25 AI Companion",
+  title: "Korn Ferry - Workforce 24 AI Companion",
   description:
     "Korn Ferry's survey of professionals around the world. It's designed to answer one simple questio",
 };
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
       <body>
         <MainComponent children={children} />
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
