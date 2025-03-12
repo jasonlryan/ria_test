cad34c08bc26497e4757a6ef193081574cd0dd0d
jasonryan-bn <jason.ryan@brilliantnoise.com>
2025-03-12 08:54:24 +0000

    Fix analytics and permissions policy issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import "../styles/bubbles.css";
 import MainComponent from "../components/MainComponent";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Korn Ferry - Workforce 24 AI Companion",
+  title: "Korn Ferry - Workforce 25 AI Companion",
   description:
-    "Korn Ferry’s survey of professionals around the world. It’s designed to answer one simple questio",
+    "Korn Ferry's survey of professionals around the world. It's designed to answer one simple questio",
 };
 
 export default function RootLayout({ children }) {
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body>
         <MainComponent children={children} />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
