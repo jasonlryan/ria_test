dd8ee774125f1c94f43bfe09821ae27a7065c8c6
Jason Ryan <jason.ryan@magnusconsulting.co.uk>
2025-03-19 14:39:15 +0000

    Update assistant ID to use environment variable

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import { redirect } from "next/navigation";
 export default function Home() {
   console.log("ROOT PAGE LOADED - REDIRECTING TO EMBED");
   // This is a server component with an immediate redirect
-  redirect("/embed/asst_f3NgFGr4gfTt6z6tCzDX6TYG");
+  redirect(
+    `/embed/${
+      process.env.OPENAI_ASSISTANT_ID || "asst_xOd6VeXqgta6bxY1LkatulKU"
+    }`
+  );
 
   // The below code won't execute due to the redirect
   return null;
