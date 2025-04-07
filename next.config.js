ee3b6fc472767ea9c726a24b03d0f77a672874c9
Jason Ryan <jason.ryan@magnusconsulting.co.uk>
2025-04-07 17:37:03 +0100

    Add outputFileTracingIncludes to next.config.js to include prompt files in deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false,path:false };
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
 
-        return config;
+    return config;
+  },
+  reactStrictMode: true,
+  output: "standalone",
+  distDir: ".next",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**": ["utils/openai/*.md"],
     },
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
