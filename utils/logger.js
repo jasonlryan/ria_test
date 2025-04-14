096f7c9b3d99efb319a05433c055beba0b90378d
Jason Ryan <jason.ryan@magnusconsulting.co.uk>
2025-04-14 13:23:10 +0100

    fixed logging error

--- utils/logger.js
+++ utils/logger.js
@@ -2,6 +2,19 @@ import { createLogger, format, transports } from "winston";
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const loggerTransports = [
+  new transports.Console({
+    level: "info", // Only show info and above in console by default
+  }),
+];
+
+// Only add file transport if not running on Vercel
+if (!process.env.VERCEL) {
+  loggerTransports.push(
+    new transports.File({ filename: "logs/query_api.log", level: "info" })
+  );
+}
+
 const logger = createLogger({
   level: process.env.LOG_LEVEL || (isProduction ? "info" : "info"),
   format: format.combine(
@@ -12,13 +25,7 @@ const logger = createLogger({
       ? format.json()
       : format.combine(format.colorize(), format.simple())
   ),
-  transports: [
-    new transports.Console({
-      level: "info", // Only show info and above in console by default
-    }),
-    new transports.File({ filename: "logs/query_api.log", level: "info" }),
-    // You can add file or remote transports here if needed
-  ],
+  transports: loggerTransports,
   exitOnError: false,
 });
 
